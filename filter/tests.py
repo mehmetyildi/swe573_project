1a7ffca1f8214198ae0b787e91faea773c1ab108
Mehmet <mehmetyildi@msn.com>
2018-12-25 11:16:06 +0300

    Ready!

--- filter/tests.py
+++ filter/tests.py
@@ -8,7 +8,72 @@ from rest_framework.test import force_authenticate
 from rest_framework.test import APIRequestFactory
 from django.test import Client
 from rest_framework import status
+from .recommender import areaRecommendation, keywordRecommendation
 
+def init():
+    Locations(name="Kadiköy",twittername="@kadikoybelediye").save()
+    Locations(name="Uskudar").save()
+    Locations(name="Beşiktaş").save()
+    Locations(name="Şişli").save()
+    Locations(name="Mecidiyeköy").save()
+    Locations(name="Maltepe").save()
+
+    Keywords(name="gitar").save()
+    Keywords(name="davul").save()
+    Keywords(name="klarnet").save()
+    Keywords(name="flut").save()
+    Keywords(name="pena").save()
+    Keywords(name="bas gitar").save()
+
+    market=Markets(name='Müzik')
+    market.save()
+
+    User(username='testuser1', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+    User(username='testuser2', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+    User(username='testuser3', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+    User(username='testuser4', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+    User(username='testuser5', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+    User(username='testuser6', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+
+    UserMarketSettings(user=User.objects.get(pk=1),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+    UserMarketSettings(user=User.objects.get(pk=2),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+    UserMarketSettings(user=User.objects.get(pk=3),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+    UserMarketSettings(user=User.objects.get(pk=4),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+    UserMarketSettings(user=User.objects.get(pk=5),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+    UserMarketSettings(user=User.objects.get(pk=6),market=Markets.objects.get(pk=1),location=Locations.objects.get(pk=1)).save()
+
+    KeywordHit(keyword=Keywords.objects.get(pk=1),setting=UserMarketSettings.objects.get(pk=1),hit="15").save()
+    KeywordHit(keyword=Keywords.objects.get(pk=2),setting=UserMarketSettings.objects.get(pk=2),hit="16").save()
+    KeywordHit(keyword=Keywords.objects.get(pk=3),setting=UserMarketSettings.objects.get(pk=3),hit="17").save()
+    KeywordHit(keyword=Keywords.objects.get(pk=4),setting=UserMarketSettings.objects.get(pk=4),hit="18").save()
+    KeywordHit(keyword=Keywords.objects.get(pk=5),setting=UserMarketSettings.objects.get(pk=5),hit="19").save()
+    KeywordHit(keyword=Keywords.objects.get(pk=6),setting=UserMarketSettings.objects.get(pk=6),hit="20").save()
+
+    AreaHit(area=Locations.objects.get(pk=1),setting=UserMarketSettings.objects.get(pk=1),hit="20").save()
+    AreaHit(area=Locations.objects.get(pk=2),setting=UserMarketSettings.objects.get(pk=2),hit="19").save()
+    AreaHit(area=Locations.objects.get(pk=3),setting=UserMarketSettings.objects.get(pk=3),hit="18").save()
+    AreaHit(area=Locations.objects.get(pk=4),setting=UserMarketSettings.objects.get(pk=4),hit="17").save()
+    AreaHit(area=Locations.objects.get(pk=5),setting=UserMarketSettings.objects.get(pk=5),hit="16").save()
+    AreaHit(area=Locations.objects.get(pk=6),setting=UserMarketSettings.objects.get(pk=6),hit="15").save()
+
+
+
+class RecommenderTests(TestCase):
+    def test_keyword_recommender(self):
+        init()
+        keywords=keywordRecommendation(UserMarketSettings.objects.get(pk=1))
+        self.assertEqual(len(keywords),5)
+        self.assertEqual(keywords[0],Keywords.objects.get(pk=6))
+        self.assertEqual(keywords[4],Keywords.objects.get(pk=2))
+        self.assertNotIn(Keywords.objects.get(pk=1),iter(keywords))
+
+    def test_area_recommender(self):
+        init()
+        areas=areaRecommendation(UserMarketSettings.objects.get(pk=1))
+        self.assertEqual(len(areas),5)
+        self.assertEqual(areas[0],Locations.objects.get(pk=1))
+        self.assertEqual(areas[4],Locations.objects.get(pk=5))
+        self.assertNotIn(Locations.objects.get(pk=6),iter(areas))
 
 class LoginPageTests(SimpleTestCase):
     def test_home_page_status_code(self):
@@ -46,32 +111,7 @@ class CreatePageTests(TestCase):
         response = views.create(request)
         self.assertEquals(response.status_code, 200)
 
-    def test_create_setting(self):
-        """
-        Ensure we can create a new account object.
-        """
-        market=Markets(name='Müzik')
-        market.save()
-        location=Locations(name='Kadıköy')
-        location.save()
-        self.user = User.objects.create(username='testuser', password='12345', is_active=True, is_staff=True, is_superuser=True)
-        self.user.set_password('hello')
-        self.user.save()
-        user=User.objects.get(username='testuser')
-        factory = APIRequestFactory()
-        request = factory.get('/filter/filters/')
-        force_authenticate(request,user=user)
-        request.user=user
-        mutable = request.POST._mutable
-        request.POST._mutable = True
-        request.method='POST'
-        request.POST['user']=user
-        request.POST['location']=location.pk
-        request.POST['market']=market.pk
-        request.POST._mutable = mutable
-        response = views.create(request)
-        self.assertEqual(UserMarketSettings.objects.count(), 1)
-        self.assertEqual(UserMarketSettings.objects.get().market.name, 'Müzik')
+
 
 class UpdatePageTests(TestCase):
     def test_update_page_status_code(self):
@@ -87,7 +127,7 @@ class UpdatePageTests(TestCase):
     def test_view_url_by_name(self):
         market=Markets(name='Müzik')
         market.save()
-        location=Locations(name='Kadıköy') 
+        location=Locations(name='Kadıköy')
         location.save()
         user = User.objects.create(username='testuser', password='12345', is_active=True, is_staff=True, is_superuser=True)
         setting=UserMarketSettings(user=user,market=market,location=location)
@@ -112,9 +152,6 @@ class UpdatePageTests(TestCase):
         self.assertEquals(response.status_code, 200)
 
     def test_update_setting(self):
-        """
-        Ensure we can create a new account object.
-        """
         market=Markets(name='Müzik')
         market.save()
         location=Locations(name='Kadıköy')
@@ -127,7 +164,7 @@ class UpdatePageTests(TestCase):
         self.user.set_password('hello')
         self.user.save()
         user=User.objects.get(username='testuser')
-        setting=UserMarketSettings(user=user,market=market,location=location)
+        setting=UserMarketSettings(user=user,market=market,location=location).save()
         factory = APIRequestFactory()
         request = factory.get('/filter/update/1/')
         force_authenticate(request,user=user)
@@ -139,6 +176,6 @@ class UpdatePageTests(TestCase):
         request.POST['location']=location2.pk
         request.POST['market']=market2.pk
         request.POST._mutable = mutable
-        response = views.create(request)
+        response = views.update(request,pk=1)
         self.assertEqual(UserMarketSettings.objects.count(), 1)
         self.assertEqual(UserMarketSettings.objects.get().market.name, 'Ayakkabı')
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
-from . twitter import resident, fetch
-
+from . twitter import resident, fetch, show_tweets
+import json
+from filter.models import Keywords, Locations
+from django.contrib.auth.models import User
+from .models import Tweet
 # Create your tests here.
 
 class TwitterTest(TestCase):
@@ -10,9 +13,25 @@ class TwitterTest(TestCase):
 
     def test_keyword(self):
         keyword="gitar"
-        result=len(fetch(keyword))
-        self.assertEqual(result,3)
+        result=fetch(keyword)
 
-    # def test_filter(self):
-    #     result=len(filter("@kadikoybelediye",fetch("gitar")))
-    #     self.assertLess(result,3)
+        for tweet in result:
+            self.assertEquals(keyword.lower() in json.dumps(tweet._json).lower(),True)
+
+    def test_tweet_fetch(self):
+        user=User(username='testuser1', password='12345', is_active=True, is_staff=True, is_superuser=True).save()
+        Locations(name="Kadiköy",twittername="@kadikoybelediye").save()
+        Keywords(name="gitar").save()
+        Keywords(name="davul").save()
+        Keywords(name="klarnet").save()
+        keywords=[]
+        areas=[]
+        for keyword in Keywords.objects.all():
+            keywords.append(keyword.name)
+        for area in Locations.objects.all():
+            areas.append(area.twittername)
+        show_tweets(user=user, areas=areas, keywords=keywords)
+        print("Tweet count: "+str(len(Tweet.objects.all())))
+        self.assertLess(len(Tweet.objects.all()),15)
+        for tweets in Tweet.objects.all():
+            self.assertEqual(resident(tweet.user.screen_name,area[0].twittername),1)
